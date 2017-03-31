7a97b49d7c088e6aa357ced427c898e4cbe450df
Enieber Cunha <eniebercunha@gmail.com>
2017-03-30 21:40:41 -0300

    add docker composer to up chat

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-const app = require('express')()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const express = require('express')
+const socket = require('socket.io')
+const r = require('rethinkdb')
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+const app = express()
+const io = socket.listen(app.listen(3000))
+console.log('App listening on 3000')
+
+const confDB = {
+  db: 'test',
+  port: 32770,
+  host: 'localhost'
+}
+
+let connection = null;
+/*r.connect(confDB, function(err, conn) {
+  if (err) throw err;
+  connection = conn;
 })
+*/
 
-io.on('connection', function(socket) {
-  console.log('Client connected ')
-
-  socket.on('disconnect', function() {
-    console.log('Client disconneted')
-  })
-
-  socket.on('client emit', function(msg) {
+io.sockets.on('connection', (sock) => {
+  console.log('connected to socket')
+  sock.on('client emit', (msg) => {
     io.emit('server emit', msg)
+  /*  r.connect(confDB)
+      .then(conn => {
+        r.table('messages')
+          .insert(msg)
+          .run(connection, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+            io.emit('server emit', msg)
+          })
+      })*/
   })
-
 })
 
-http.listen(3000, function() {
-  console.log('listener in localhost:3000')
-})
+app.use(express.static(__dirname + "/public"))
